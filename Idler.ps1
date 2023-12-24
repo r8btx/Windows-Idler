e1223268e0c5cc710c6db676a571d00fc3564b7b
r8btx <60275558+r8btx@users.noreply.github.com>
2023-12-24 09:08:59 -0500

    rewrite

--- Idler.ps1
+++ Idler.ps1
@@ -1,84 +1,92 @@
-# This script prevents low-threat individuals (e.g. kids) from messing with your computer while leaving programs running in the background.
-# Upon execution, the script does the following:
-#   1. Turn off the screen
-#   2. Lock down Windows (require authentication to log back on)
-#   3. Prevent sleep and hibernation
-#   4. If Windows is unlocked, the script will shut down automatically
+<#
+    Upon execution, the script does the following:
+      1. Turns off the screen
+      2. Locks Windows (requires authentication to unlock)
+      3. Prevents sleep and hibernation modes
+      4. Shuts down if unlocked
+    To create a shortcut: cmd /c PowerShell Set-ExecutionPolicy Bypass -Scope Process;$path='SCRIPT_FOLDER\Idler.ps1';^& $path
+#>
 
-# If you want to create a shortcut to this script, try the following:
-# cmd /c PowerShell Set-ExecutionPolicy Bypass -Scope Process;$path='FOLDER CONTAINING THE SCRIPT\Idler.ps1';^& $path
+[CmdletBinding()]
+param()
 
-
-Write-Host "Setting up Idler..."
-
-# Display off @https://powershellmagazine.com/2013/07/18/pstip-how-to-switch-off-display-with-powershell/
-Add-Type -TypeDefinition '
-using System;
-using System.Runtime.InteropServices;
- 
-namespace Utilities {
-   public static class Display
-   {
-      [DllImport("user32.dll", CharSet = CharSet.Auto)]
-      private static extern IntPtr SendMessage(
-         IntPtr hWnd,
-         UInt32 Msg,
-         IntPtr wParam,
-         IntPtr lParam
-      );
- 
-      public static void PowerOff ()
-      {
-         SendMessage(
-            (IntPtr)0xffff, // HWND_BROADCAST
-            0x0112,         // WM_SYSCOMMAND
-            (IntPtr)0xf170, // SC_MONITORPOWER
-            (IntPtr)0x0002  // POWER_OFF
-         );
-      }
-   }
+# Gets the last Windows logon timestamp
+function Get-LastLogonString {
+    return (Get-WmiObject -Class Win32_NetworkLoginProfile | 
+        Where-Object { $_.Caption -eq [Environment]::UserName }).LastLogon
 }
-'
-# Lock Workstation @http://gallery.technet.microsoft.com/ScriptCenter/en-us/a2178d49-79cd-4b3d-918a-47e9c6800262
-Function Lock-Workstation {
+
+# Locks the Windows workstation
+# @http://gallery.technet.microsoft.com/ScriptCenter/en-us/a2178d49-79cd-4b3d-918a-47e9c6800262
+function Lock-Workstation {
     $signature = @'
     [DllImport("user32.dll", SetLastError = true)]
     public static extern bool LockWorkStation();
 '@
 
-    $LockWorkStation = Add-Type -memberDefinition $signature -name "Win32LockWorkStation" -namespace Win32Functions -passthru
-    $LockWorkStation::LockWorkStation() | Out-Null
+    $lockWorkStation = Add-Type -MemberDefinition $signature -Name "Win32LockWorkStation" -Namespace Win32Functions -PassThru
+    $lockWorkStation::LockWorkStation() | Out-Null
 }
 
-# Staying Awake @https://gist.github.com/CMCDragonkai/bf8e8b7553c48e4f65124bc6f41769eb
-$Code=@'
-[DllImport("kernel32.dll", CharSet = CharSet.Auto,SetLastError = true)]
-public static extern void SetThreadExecutionState(uint esFlags);
+Write-Host "Initializing Idler..."
+
+# Type definition to turn off the display
+# @https://powershellmagazine.com/2013/07/18/pstip-how-to-switch-off-display-with-powershell/
+Add-Type -TypeDefinition @'
+using System;
+using System.Runtime.InteropServices;
+ 
+namespace Utilities {
+    public static class Display {
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern IntPtr SendMessage(
+            IntPtr hWnd,
+            UInt32 Msg,
+            IntPtr wParam,
+            IntPtr lParam
+        );
+ 
+        public static void PowerOff() {
+            SendMessage(
+                (IntPtr)0xffff, // HWND_BROADCAST
+                0x0112,         // WM_SYSCOMMAND
+                (IntPtr)0xf170, // SC_MONITORPOWER
+                (IntPtr)0x0002  // POWER_OFF
+            );
+        }
+    }
+}
 '@
 
-$ste = Add-Type -memberDefinition $Code -name System -namespace Win32 -passThru
+# Type definition for maintaining system awake
+# @https://gist.github.com/CMCDragonkai/bf8e8b7553c48e4f65124bc6f41769eb
+$code = @'
+[DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+public static extern void SetThreadExecutionState(uint esFlags);
+'@
 
 # Requests that the other EXECUTION_STATE flags set remain in effect until
 # SetThreadExecutionState is called again with the ES_CONTINUOUS flag set and
 # one of the other EXECUTION_STATE flags cleared.
+$ste = Add-Type -MemberDefinition $code -Name System -Namespace Win32 -PassThru
 $ES_CONTINUOUS = [uint32]"0x80000000"
 $ES_SYSTEM_REQUIRED = [uint32]"0x00000001"
 
+$lastLogonString = Get-LastLogonString
+
 try {
-
-    Write-Host "Starting Idler..."
-
+    Write-Host "Activating Idler..."
     [Utilities.Display]::PowerOff()
     Lock-Workstation
     $ste::SetThreadExecutionState($ES_CONTINUOUS -bor $ES_SYSTEM_REQUIRED)
+
     do {
         Start-Sleep -Seconds 1
-    } until (!(($tasklist | Where Name -EQ winlogon).Threads.WaitReason -contains 'LpcReply') -and ('LogonUI' -inotin $tasklist.Name))
+    } until ($lastLogonString -ne (Get-LastLogonString))
 
-} finally {
-
-    Write-Host "Stopping Staying Awake..."
+}
+finally {
+    Write-Host "Deactivating Idler..."
     $ste::SetThreadExecutionState($ES_CONTINUOUS)
     Write-Host "STOPPED."
 }
-
